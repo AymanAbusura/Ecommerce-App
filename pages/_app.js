af20ec3f9e2de965d7646ff6e8d06661a737547b
Ayman Abusura <aymansalameh1996@hotmail.com>
2023-09-05 18:04:21 +0300

    Done

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import Layout from '../components/Layout'
+import '../styles/globals.css'
+import { StateContext } from '../context/StateContext';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
+}
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'u6ty06da',
+    dataset: 'production'
+  }
+})
